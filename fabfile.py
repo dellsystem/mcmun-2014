647aabff3ca7cfe53e21b80f602bbeeae382ea1b
dellsystem <ilostwaldo@gmail.com>
2012-03-29 12:25:48 -0400

    Add deployment commands to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,24 @@
-from fabric.api import local, run
+from fabric.api import local, run, env
 
-def less():
+env.hosts = ['mcmun@mcmun.org']
+
+FILES = ['index.html', 'css/splash.css', 'img/dove.png', 'img/email.png', 'img/facebook.png', 'img/logo.png', 'img/twitter.png', 'css/fonts/*']
+
+def prepare():
 	local('lessc css/splash.less -x > css/splash.css')
 
+def archive():
+	local('tar czvf mcmun.tar.gz %s' % ' '.join(FILES))
+
+def transfer():
+	local('scp mcmun.tar.gz %s:mcmun.org/ROOT' % env.hosts[0])
+	local('rm mcmun.tar.gz')
+
+def unpack():
+	run('cd mcmun.org/ROOT; tar xvzf mcmun.tar.gz')
+
 def deploy():
-	pass
+	prepare()
+	archive()
+	transfer()
+	unpack()
