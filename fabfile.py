112d2a9208835df334154ee95873855a2aede59a
dellsystem <ilostwaldo@gmail.com>
2012-05-27 23:28:07 -0400

    Add new deploy method to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,12 @@
-from fabric.api import local, run
+from fabric.api import local, run, env
+
+env.hosts = ['dellsystem@mcmun.org']
 
 def less():
 	local("lessc mcmun/static/css/mcmun.less -X > mcmun/static/css/mcmun.css")
 
 def deploy():
-	pass
+	less()
+	local('git push')
+	run('cd mcmun.org && git pull')
+	run('echo "yes" | python manage.py collectstatic')
