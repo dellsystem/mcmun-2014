e596c733a16958641381fe7725499360d90ee276
dellsystem <ilostwaldo@gmail.com>
2012-05-28 23:42:05 -0400

    Add carousel functionality
    Now with auto-rotation and some text shown over the image.

--- mcmun/static/js/mcmun.js
+++ mcmun/static/js/mcmun.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
+	var rotateTimeout = 3000;
+	var firstDiv = $('#carousel-blocks div')[0];
+	var timeout;
+
 	$('#carousel-blocks div').click(function () {
+		$('.active').removeClass();
+		$(this).addClass('active');
 		$('#carousel-image').removeClass().addClass($(this).attr('data-image'));
 	});
+
+
+	var rotateCarousel = function () {
+		var nextDiv = $('#carousel-blocks .active').next()[0] || firstDiv;
+		$(nextDiv).click();
+		timeout = setTimeout(rotateCarousel, rotateTimeout);
+	};
+
+	timeout = setTimeout(rotateCarousel, rotateTimeout);
 });
