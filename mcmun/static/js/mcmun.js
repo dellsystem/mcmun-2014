620c595ca3dfd050ed6e44563f0d8dfad6b5cd76
dellsystem <ilostwaldo@gmail.com>
2012-06-14 21:31:59 -0400

    Show secretariat titles upon mouseover

--- mcmun/static/js/mcmun.js
+++ mcmun/static/js/mcmun.js
@@ -20,6 +20,17 @@ $(document).ready(function() {
 
 	timeout = setTimeout(rotateCarousel, rotateTimeout);
 
+	// Show the person's title upon hovering over the photo
+	$('#sec-bios').delegate('.photo', 'mouseenter', function (event) {
+		var title = $(this).next().find('h3').text();
+		$(this).append('<div class="title-hover">' + title + '</div>');
+		$('.title-hover').fadeIn(300);
+	});
+
+	$('#sec-bios').delegate('.photo', 'mouseleave', function (event) {
+		$('.title-hover').remove();
+	});
+
 	$('#sec-bios').delegate('.photo', 'click', function (event) {
 		$('.active').removeClass('active');
 		$('.bio').hide();
