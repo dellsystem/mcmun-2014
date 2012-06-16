51a03b5ee484e979fa84e7ec03bd8a19cbee5d5b
dellsystem <ilostwaldo@gmail.com>
2012-06-16 16:17:30 -0400

    Update payment page, by request
    Made the top navigation bar horizontal rather than vertical. I
    initially wanted to put in the '::' using CSS (:after) but it proved
    to be too much trouble because of the initial li and anyway this method
    probably has better browser support.
    
    Also added a rudimentary js "fee calculator". Was kind of fun. This
    required adding a template filter for ranges.

--- mcmun/static/js/mcmun.js
+++ mcmun/static/js/mcmun.js
@@ -36,4 +36,39 @@ $(document).ready(function() {
 		$('.bio').hide();
 		$(this).addClass('active').next().show();
 	});
+
+	var delegationFee = 75;
+
+	$('#fee-calculator').delegate('select', 'change', function (event) {
+		var numDelegates = parseInt($('#num-delegates option:checked').val(), 10);
+		var registrationType = $('#registration-type option:checked').val();
+
+		// Only show the fee information stuff when everything has been selected
+		if (numDelegates > 0 && registrationType !== '') {
+			var delegateFee, totalFee, deposit, remainder;
+
+			switch (registrationType) {
+				case 'priority':
+					delegateFee = 80;
+				break;
+				case 'regular':
+					delegateFee = 95;
+				break;
+				case 'international':
+					delegateFee = 50;
+				break;
+			}
+
+			if (delegateFee) {
+				totalFee = numDelegates * delegateFee + delegationFee;
+				deposit = delegationFee + (numDelegates * delegateFee) * 0.5;
+				remainder = totalFee - deposit;
+				$('#fee-information').text('Your total fee, for ' + numDelegates + ' delegates and ' + registrationType + ' registration, is $' + totalFee.toFixed(2) + '. If you wish to pay using the tiered system, your deposit would be $' + deposit.toFixed(2) + ', and the remainder would be $' + remainder.toFixed(2) + '.');
+			} else {
+				// Someone is mucking about with the form
+				$('#fee-information').text("Please stop messing with the form. There's nothing interesting here.");
+			}
+			$('#fee-information').show();
+		}
+	});
 });
