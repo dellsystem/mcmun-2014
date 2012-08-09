9473da6c9ff7e81c5f7a4fe1d4b8cde80a63432a
dellsystem <ilostwaldo@gmail.com>
2012-08-09 14:24:24 -0400

    Add "Change password" feature
    Also made the form.errors on the login page show up within the properly
    coloured fieldset (fieldset.errors)

--- mcmun/urls.py
+++ mcmun/urls.py
@@ -14,5 +14,7 @@ urlpatterns = patterns('',
     url(r'^registration', 'mcmun.views.registration', name='registration'),
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    url(r'^password$', 'django.contrib.auth.views.password_change', {'template_name': 'password.html'}, name='password'),
+    url(r'^password_success$', 'django.contrib.auth.views.password_change_done', {'template_name': 'password_success.html'}, name="password_success"),
     url(r'^', include('cms.urls')),
 )
