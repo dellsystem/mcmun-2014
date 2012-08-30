9cafa8e5c2afa842f522ab9978614bb5c9e319b6
dellsystem <ilostwaldo@gmail.com>
2012-08-30 00:55:41 -0400

    Show dais member photos on committee pages
    This took, approximately, a really long time

--- committees/views.py
+++ committees/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.conf import settings
+
 from committees.models import Committee
 
+
 def view(request, slug):
-    try:
-        committee = Committee.objects.get(slug=slug)
-    except Committee.DoesNotExist:
-        raise Http404
+	try:
+		committee = Committee.objects.get(slug=slug)
+	except Committee.DoesNotExist:
+		raise Http404
 
-    data = {
-        'committee': committee,
-    }
+	data = {
+		'page': {
+			'long_name': committee.name,
+		},
+		'committee': committee,
+		'dais_template': 'dais_photos/%s.html' % committee.slug,
+		'DAIS_PHOTO_URL': '%simg/dais/%s/' % (settings.STATIC_URL, committee.slug),
+	}
 
-    return render(request, 'committee.html', data)
+	return render(request, 'committee.html', data)
