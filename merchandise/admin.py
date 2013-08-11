fb609a25357ea7c79ad49f73e21ca0dd16fa6699
dellsystem <ilostwaldo@gmail.com>
2013-08-11 05:46:46 +0000

    Show BundleOrder, ItemOrder in admin

--- merchandise/admin.py
+++ merchandise/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from merchandise.models import Bundle, Item
+from merchandise.models import Bundle, Item, BundleOrder, ItemOrder
 
 
 admin.site.register(Bundle)
 admin.site.register(Item)
+admin.site.register(BundleOrder)
+admin.site.register(ItemOrder)
