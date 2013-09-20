b3670721605af48a7196c69773c1bfc5bf54aaa3
dellsystem <ilostwaldo@gmail.com>
2013-09-20 22:47:45 +0000

    Show num delegates for RegisteredSchools in admin

--- mcmun/admin.py
+++ mcmun/admin.py
@@ -7,7 +7,7 @@ from mcmun.models import RegisteredSchool, ScholarshipApp, SecretariatMember, \
 class RegisteredSchoolAdmin(admin.ModelAdmin):
 	# Sort reverse chronologically
 	ordering = ['-id']
-	list_display = ('school_name', 'email', 'is_approved', 'amount_owed', 'amount_paid')
+	list_display = ('school_name', 'email', 'is_approved', 'num_delegates', 'amount_owed', 'amount_paid')
 	list_filter = ('is_approved', 'use_online_payment')
 	exclude = ('account',)
 
