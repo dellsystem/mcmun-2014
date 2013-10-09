7ade89aba80b14fb30d56983aa45f2f313730259
dellsystem <ilostwaldo@gmail.com>
2013-10-09 02:08:40 +0000

    Add confirmation step for renegenerate_invoice
    Now the id is taken using raw_input.

--- fabfile.py
+++ fabfile.py
@@ -34,5 +34,5 @@ def static():
 def restart():
     local('kill -HUP `cat /tmp/gunicorn.pid`')
 
-def regen(school_id):
-    local('python manage.py regenerate_invoice %s' % school_id)
+def regen():
+    local('python manage.py regenerate_invoice')
