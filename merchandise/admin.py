cf00875d3bc679be26c9055bbf77ce3781977df5
dellsystem <ilostwaldo@gmail.com>
2013-12-29 08:46:55 +0000

    Improve BundleOrder admin interface
    Now shows ItemOrder inlines and displays all the field info for
    list_display.

--- merchandise/admin.py
+++ merchandise/admin.py
@@ -8,7 +8,16 @@ class ItemOrderAdmin(admin.ModelAdmin):
     ordering = ['school']
 
 
+class ItemOrderInline(admin.StackedInline):
+    model = ItemOrder
+
+
+class BundleOrderAdmin(admin.ModelAdmin):
+    inlines = [ItemOrderInline]
+    list_display = ('bundle', 'school', 'quantity', 'comment')
+
+
 admin.site.register(Bundle)
 admin.site.register(Item)
-admin.site.register(BundleOrder)
+admin.site.register(BundleOrder, BundleOrderAdmin)
 admin.site.register(ItemOrder, ItemOrderAdmin)
