8bf5905f9f084595ed8fcc75206fe2c153a2d00c
dellsystem <ilostwaldo@gmail.com>
2014-01-03 03:57:01 -0400

    Refactor urlpatterns in committees/urls.py

--- committees/urls.py
+++ committees/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 
+SLUG = r'^(?P<slug>[a-zA-Z0-9-]+)'
+
 urlpatterns = patterns('',
     # Redirect committees/ back to committees (show the page)
     url(r'^$', 'cms.views.main', name='committees'),
-    url(r'^(?P<slug>[a-zA-Z0-9-]+)/apply$', 'committees.views.application', name='committee_app'),
-    url(r'^(?P<slug>[a-zA-Z0-9-]+)/papers$', 'committees.views.list_papers', name='list_papers'),
-    url(r'^(?P<slug>[a-zA-Z0-9-]+)', 'committees.views.view', name='committee_view'),
+    url(SLUG + '/apply$', 'committees.views.application', name='committee_app'),
+    url(SLUG + '/papers$', 'committees.views.list_papers', name='list_papers'),
+    url(SLUG, 'committees.views.view', name='committee_view'),
 )
