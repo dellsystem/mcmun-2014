c66fafad03c5999fa670cfe430eb362ad4ea0f78
dellsystem <ilostwaldo@gmail.com>
2014-01-13 00:30:48 -0500

    Enable searching for Committee/DelegateAssignments
    In the admin panel.

--- committees/admin.py
+++ committees/admin.py
@@ -5,10 +5,17 @@ from committees.models import *
 
 class CommitteeAssignmentAdmin(admin.ModelAdmin):
     list_display = ('school', 'committee', 'assignment')
+    search_fields = ('school__school_name', 'committee__name', 'assignment')
 
 
 class DelegateAssignmentAdmin(admin.ModelAdmin):
     list_display = ('school', 'committee', 'committee_assignment', 'delegate_name')
+    search_fields = (
+        'committee_assignment__school__school_name',
+        'committee_assignment__committee__name',
+        'committee_assignment__assignment',
+        'delegate_name'
+    )
 
     def school(self, obj):
         return "%s" % obj.committee_assignment.school
