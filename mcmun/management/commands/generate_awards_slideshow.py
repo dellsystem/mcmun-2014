9f3ec14beeef7daef16e50df60b5e9bdf4751f5f
dellsystem <ilostwaldo@gmail.com>
2014-01-20 02:23:12 -0500

    Add best delegations to awards slideshow
    Automatically generated

--- mcmun/management/commands/generate_awards_slideshow.py
+++ mcmun/management/commands/generate_awards_slideshow.py
@@ -1,10 +1,62 @@
 import codecs
+from collections import defaultdict
+from operator import itemgetter
 from xml.dom import minidom
 from xml.etree.ElementTree import tostring
 
 from django.core.management.base import BaseCommand, CommandError
 
 from committees.models import AwardAssignment, Committee
+from mcmun.models import RegisteredSchool as S
+
+
+def get_highest_value(d):
+    """Sort the dictionary by value and return the top entry's key."""
+    return sorted(d.items(), key=itemgetter(1))[0][0]
+
+def calculate_best_delegations():
+    award_points = {
+        'Best Delegate': 5,
+        'Outstanding Delegate': 4,
+        'Honorable Mention': 3,
+        'Book Award': 1,
+    }
+
+    # Calculate the points, and the number of winners, for each school
+    school_points = defaultdict(int)
+    school_winners = defaultdict(int)
+    for award in AwardAssignment.objects.filter(position__isnull=False):
+        num_points = award_points[award.award.name]
+        school = award.position.school
+        school_points[school.school_name] += num_points
+        school_winners[school.school_name] += 1
+
+    # Figure out the totals for each school
+    small_totals = {}
+    large_totals = {}
+    for school_name in school_points:
+        num_points = school_points[school_name]
+        num_winners = school_winners[school_name]
+        num_delegates = S.objects.get(school_name=school_name).num_delegates
+        total = (num_points + num_winners) / float(num_delegates)
+
+        print school_name,
+        print "%d points, %d winners, %d delegates; %.2f total" % (
+            num_points, num_winners, num_delegates, total)
+
+        if 2 <= num_delegates <= 15:
+            small_totals[school_name] = total
+            print "small delegation"
+        elif num_delegates >= 16:
+            large_totals[school_name] = total
+            print "large delegation"
+
+    # Sort them by value to find the best large and small delegations
+    best_large = get_highest_value(large_totals)
+    best_small = get_highest_value(small_totals)
+
+    return best_large, best_small
+
 
 
 class Command(BaseCommand):
@@ -28,33 +80,50 @@ class Command(BaseCommand):
                 # This is a really hacky workaround to using a regex
                 if label.endswith('!'):
                     committee_name = label[:-2]
-                else:
-                    # Don't need to change anything, it's not a committee
-                    continue
 
-                if committee_name in committees_dict:
-                    committee = committees_dict[committee_name]
-                    # Fill in the context dict (for string replacements)
-                    awards = committee.awards.order_by('-pk')
+                    if committee_name in committees_dict:
+                        committee = committees_dict[committee_name]
+                        # Fill in the context dict (for string replacements)
+                        awards = committee.awards.order_by('-pk')
+                        context = {}
+                        for i, award in enumerate(awards):
+                            j = i + 1
+                            if award.position:
+                                delegate_name = award.position.assignment
+                                school_name = award.position.school.school_name
+                            else:
+                                delegate_name = 'Wendy Liu'
+                                school_name = 'McGill University'
+
+                            context['award_name_%d' % j] = award.award.name
+                            context['delegate_name_%d' % j] = delegate_name
+                            context['school_name_%d' % j] = school_name
+
+                        for flowPara in g.getElementsByTagName('flowPara'):
+                            text = flowPara.firstChild
+                            if text:
+                                new_value = text.nodeValue % context
+                                text.replaceWholeText(new_value)
+                elif label.endswith('?'):
+                    # The last 2 slides at the end, best large/small delegation
+                    # Really hacky I know
+                    best_large, best_small = calculate_best_delegations()
+                    print "Winners: %s (large) and %s (small)" % (best_large,
+                        best_small)
                     context = {}
-                    for i, award in enumerate(awards):
-                        j = i + 1
-                        if award.position:
-                            delegate_name = award.position.assignment
-                            school_name = award.position.school.school_name
-                        else:
-                            delegate_name = 'Wendy Liu'
-                            school_name = 'McGill University'
-
-                        context['award_name_%d' % j] = award.award.name
-                        context['delegate_name_%d' % j] = delegate_name
-                        context['school_name_%d' % j] = school_name
+                    context['best_large'] = best_large
+                    context['best_small'] = best_small
 
                     for flowPara in g.getElementsByTagName('flowPara'):
                         text = flowPara.firstChild
                         if text:
                             new_value = text.nodeValue % context
                             text.replaceWholeText(new_value)
+
+                    continue
+                else:
+                    # Don't need to change anything, it's not a committee
+                    continue
 
         # Save the updated SVG
         output_file = codecs.open('updated_awards.svg', 'w', encoding='utf-8')
