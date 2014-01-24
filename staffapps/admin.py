127070e61b3ff9cde14c2eae574456917d5eaab1
dellsystem <ilostwaldo@gmail.com>
2014-01-23 21:17:32 -0500

    Improve admin pages for staffapps models
    Page coords need to see phone numbers, etc

--- staffapps/admin.py
+++ staffapps/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from staffapps.models import CoordinatorApp, LogisticalApp, CommitteesApp
 
 
-admin.site.register(CoordinatorApp)
-admin.site.register(LogisticalApp)
-admin.site.register(CommitteesApp)
+class StaffAppAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone_number')
+    search_Fields = ('full_name',)
+
+
+admin.site.register(CoordinatorApp, StaffAppAdmin)
+admin.site.register(LogisticalApp, StaffAppAdmin)
+admin.site.register(CommitteesApp, StaffAppAdmin)
