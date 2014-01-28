f4c1e0498cf908b14009fc8dbc983d92fd4c5380
dellsystem <ilostwaldo@gmail.com>
2014-01-28 01:28:04 -0500

    Add command for exporting honourable mentions

---
+++ mcmun/management/commands/export_awards.py
@@ -0,0 +1,30 @@
+import csv
+import sys
+
+from django.core.management.base import BaseCommand, CommandError
+
+from committees.models import AwardAssignment
+
+
+class Command(BaseCommand):
+    help = "Exports the awards for use in printing certificates."
+
+    def handle(self, **options):
+        field_names = [
+            'Committee',
+            'Position',
+            'Delegatename',
+            'University',
+        ]
+        writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
+        writer.writerow(field_names)
+        awards = AwardAssignment.objects.filter(award__name='Honorable Mention')
+        for instance in awards:
+            for delegate in instance.position.delegateassignment_set.all():
+                row = [
+                    instance.committee.name,
+                    instance.position.assignment,
+                    delegate.delegate_name,
+                    instance.position.school,
+                ]
+                writer.writerow([unicode(field).encode('utf-8') for field in row])
